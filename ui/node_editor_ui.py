27f9afcba8df4746af25a489b0788ba1730fb159
strike-digital <a@sunadal.co.uk>
2022-10-21 22:55:21 +0100

    Added some extra things to the node editor panel

--- ui/node_editor_ui.py
+++ ui/node_editor_ui.py
@@ -14,23 +14,29 @@ class STRIKE_PT_node_panel(bpy.types.Panel):
         layout = self.layout
         node = context.active_node
 
-        box1 = layout.box()
-        box = box1.column()
+        box = layout.column()
         box.use_property_split = True
         box.use_property_decorate = True
         # box.prop(node, "name", text="Name")
         # box.prop(node, "label", text="Label")
         box.prop(node, "type", text="Type")
         box.prop(node, "bl_idname", text="Id name")
+        box.separator()
         box.prop(node, "dimensions")
         box.prop(node, "width", text="Width")
         box.prop(node, "height", text="Height")
         box.prop(node, "location", text="Location")
+        box.separator()
         box.prop(node, "hide", toggle=True)
         box.prop(node, "use_custom_color", text="Use custom color", toggle=True)
         box.prop(node, "color", text="Color")
+        if node.type == "FRAME":
+            box.separator()
+            box.prop(node, "label_size", text="Label size")
+            box.prop(node, "shrink", text="Shrink to minimum", toggle=True)
+            box.prop(node, "text")
 
-        row = box1.row()
+        row = layout.row()
         row.scale_y = 1.5
         op = row.operator("wm.context_set_string", text="Copy data path", icon="FILE_SCRIPT")
         op.data_path = "window_manager.clipboard"
