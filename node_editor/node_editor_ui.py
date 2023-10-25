7ce120e071c69952c663008e3963a70cfabf5741
Strike Digital <59890307+strike-digital@users.noreply.github.com>
2023-10-25 21:26:07 +0100

    Updated node info

--- node_editor/node_editor_ui.py
+++ node_editor/node_editor_ui.py
@@ -1,12 +1,23 @@
 import bpy
-from bpy.types import Menu
-from .operators.op_align_nodes_axis import STRIKE_OT_align_nodes_axis
+from bpy.types import Menu, UILayout
+
 from ..btypes import BMenu, BPanel
+from ..settings.prop_window_manager import get_strike_settings
+from .operators.op_align_nodes_axis import STRIKE_OT_align_nodes_axis
+
+
+class STRIKE_UL_inputs(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        layout: UILayout = layout
+        layout.prop(item, "name", text="", emboss=False)
+        row = layout.row(align=True)
+        row.alignment = "RIGHT"
+        idx = str(list(data.inputs).index(item))
+        row.label(text=f"{idx}")
 
 
 @BPanel(space_type="NODE_EDITOR", region_type="UI", label="Info", parent="NODE_PT_active_node_generic")
 class STRIKE_PT_node_panel(bpy.types.Panel):
-
     @classmethod
     def poll(cls, context):
         if context.active_node:
@@ -15,15 +26,25 @@ class STRIKE_PT_node_panel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         node = context.active_node
-        row = layout.row(align=True)
-        row.operator("strike.align_nodes")
-        row.scale_y = 2
 
-        box = layout.column()
+        box = layout.box().column()
         box.use_property_split = True
         box.use_property_decorate = True
-        # box.prop(node, "name", text="Name")
-        # box.prop(node, "label", text="Label")
+
+        # row = box.row()
+        row = box.row(align=True)
+
+        sub = row.row(align=True)
+        sub.alignment = "CENTER"
+        sub.label(text="       Node")
+
+        sub = row.row(align=True)
+        op = sub.operator("wm.context_set_string", text="", icon="COPYDOWN", emboss=False)
+        op.data_path = "window_manager.clipboard"
+        op.value = ".".join([repr(node.id_data), node.path_from_id()])
+
+        box.separator()
+
         box.prop(node, "type", text="Type")
         box.prop(node, "bl_idname", text="Id name")
         box.separator()
@@ -41,16 +62,34 @@ class STRIKE_PT_node_panel(bpy.types.Panel):
             box.prop(node, "shrink", text="Shrink to minimum", toggle=True)
             box.prop(node, "text")
 
-        row = layout.row()
-        row.scale_y = 1.5
-        op = row.operator("wm.context_set_string", text="Copy data path", icon="FILE_SCRIPT")
-        op.data_path = "window_manager.clipboard"
-        op.value = ".".join([repr(node.id_data), node.path_from_id()])
+        box = layout.box().column()
+        box.use_property_split = True
+        box.use_property_decorate = True
+
+        row = box.row(align=True)
+        row.alignment = "CENTER"
+        row.label(text="Inputs")
+
+        strike = get_strike_settings(context)
+        box.template_list("STRIKE_UL_inputs", "", node, "inputs", strike, "node_input_idx")
+        socket = node.inputs[strike.node_input_idx]
+
+        box.separator()
+        box.prop(socket, "default_value", text="Value")
+        box.prop(socket, "name", text="Display name")
+        box.prop(socket, "identifier", text="Identifier")
+        box.prop(socket, "bl_idname")
+        box.prop(socket, "description", text="Description")
+        box.prop(socket, "display_shape", text="Shape")
+        box.prop(socket, "type")
+        box.prop(socket, "enabled", text="Enabled")
+        box.prop(socket, "hide", text="Hide")
+        box.prop(socket, "hide_value", text="Hide value")
+        box.prop(socket, "is_linked")
 
 
 @BMenu()
 class STRIKE_MT_align_menu_pie(Menu):
-
     @classmethod
     def poll(cls, context):
         if not context.space_data or context.area.type != "NODE_EDITOR":
---
+++ settings/prop_window_manager.py
@@ -0,0 +1,23 @@
+import bpy
+from bpy.props import IntProperty
+from bpy.types import Context, PropertyGroup
+
+from ..btypes import BPropertyGroup
+
+
+@BPropertyGroup(bpy.types.WindowManager, "strike_tools")
+class StrikeSettings(PropertyGroup):
+
+    # The index of the input being viewed
+    def get_idx(self):
+        node = bpy.context.active_node
+        return min(self.get("_node_input_idx", 0), len(node.inputs) - 1)
+
+    def set_idx(self, value: int):
+        self["_node_input_idx"] = value
+
+    node_input_idx: IntProperty(get=get_idx, set=set_idx)
+
+
+def get_strike_settings(context: Context) -> StrikeSettings:
+    return context.window_manager.strike_tools
